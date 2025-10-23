56ae94efebcf38c9cb3ac107b230a45248c4bc56
nathanielnwl <2103306@sit.singaporetech.edu.sg>
2025-10-23 15:58:27 +0800

    Balance Dashboard v1.0
    Found a template
    
    gotta add in transaction history and the show all bank balances relating to the user

---
+++ public/dashboard.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login | NexaBank</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <?php include '../includes/dbconnection.php';?>
+  </head>
+<body>
+  <div class="wrapper">
+    <h2>Welcome !! </h2>
+      <div class="input-box button">
+      
+  </div>
+</h2>
+  </div>
+</body>
+</html>
