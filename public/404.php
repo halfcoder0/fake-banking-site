750315d547f5e009da204e97b5e36c505817181b
Ambrose <51565692+halfcoder0@users.noreply.github.com>
2025-10-26 20:12:31 +0800

    Update 404.php

--- public/404.php
+++ public/404.php
@@ -1,16 +1,7 @@
 <?php
 require_once('../controllers/helpers.php');
-
-$script_hashes = [
-    'sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=',
-    'sha256-98vAGjEDGN79TjHkYWVD4s87rvWkdWLHPs5MC3FvFX4=',
-    'sha256-C8oQVJ33cKtnkARnmeWp6SDChkU+u7KvsNMFUzkkUzk='
-];
-$style_hashes = [
-    'sha256-SYaExfXhqspc1072C5RaeBUps+HeQgzFAdn1JcknIo8='
-];
 $nonce = generate_random();
-add_csp_header($script_hashes, $style_hashes, $nonce);
+add_csp_header($nonce);
 
 ?>
 <!DOCTYPE html>
@@ -27,7 +18,7 @@ add_csp_header($script_hashes, $style_hashes, $nonce);
     <link rel="icon" type="image/png" sizes="16x16" href="./assets/images/favicon.png">
     <title>Nexabank | 404</title>
     <!-- Custom CSS -->
-    <link href="./dist/css/style.min.css" rel="stylesheet" integrity="sha256-SYaExfXhqspc1072C5RaeBUps+HeQgzFAdn1JcknIo8=" crossorigin="anonymous">
+    <link href="./dist/css/style.min.css" rel="stylesheet" nonce="<?= htmlspecialchars($nonce, ENT_QUOTES) ?>" crossorigin="anonymous">
 </head>
 
 <body>
@@ -48,10 +39,10 @@ add_csp_header($script_hashes, $style_hashes, $nonce);
         </div>
     </div>
     <!-- All Required js -->
-    <script src="./assets/libs/jquery/dist/jquery.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="./assets/libs/jquery/dist/jquery.min.js" nonce="<?= htmlspecialchars($nonce, ENT_QUOTES) ?>" crossorigin="anonymous"></script>
     <!-- Bootstrap tether Core JavaScript -->
-    <script src="./assets/libs/popper.js/dist/umd/popper.min.js" integrity="sha256-98vAGjEDGN79TjHkYWVD4s87rvWkdWLHPs5MC3FvFX4=" crossorigin="anonymous"></script>
-    <script src="./assets/libs/bootstrap/dist/js/bootstrap.min.js" integrity="sha256-C8oQVJ33cKtnkARnmeWp6SDChkU+u7KvsNMFUzkkUzk=" crossorigin="anonymous"></script>
+    <script src="./assets/libs/popper.js/dist/umd/popper.min.js" nonce="<?= htmlspecialchars($nonce, ENT_QUOTES) ?>" crossorigin="anonymous"></script>
+    <script src="./assets/libs/bootstrap/dist/js/bootstrap.min.js" nonce="<?= htmlspecialchars($nonce, ENT_QUOTES) ?>" crossorigin="anonymous"></script>
     <!-- This page plugin js -->
     <script nonce="<?= htmlspecialchars($nonce, ENT_QUOTES) ?>">
         $('[data-toggle="tooltip"]').tooltip();
